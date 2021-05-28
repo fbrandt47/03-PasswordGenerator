811222fd7a882494849cc5caa53838010a2586dc
fredericka.ls.brandt@gmail.com <fredericka.ls.brandt@gmail.com>
2021-05-28 07:42:47 -0500

    Homwework 03 DONE

--- logic.js
+++ logic.js
@@ -30,7 +30,7 @@ var specialCharacters = [
 var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 // Array of lowercase characters to be included in password
-var lowerCasedCharacters = [
+var lowerCaseCharacters = [
   "a",
   "b",
   "c",
@@ -59,7 +59,7 @@ var lowerCasedCharacters = [
   "z"
 ];
 
-var uppcaseLetters = [ "A", "B", "C", "D"]
+var upperCaseCharacters = [ "A", "B", "C", "D", "E"]
 
 
 
@@ -94,14 +94,29 @@ function getPasswordOptionsFromUser() {
   console.log(hasSpecialCharacters);
 
   //confirm whether or not they want all the OTHER character types 
+  var hasNumericCharacters = confirm("Click okay if you want to use numbers");
 
-  //make sure that the user chose some type of character 
+  var hasLowerCaseCharacters = confirm("Click okay if you want to use lowercase letters");
+
+  var hasUpperCaseCharacters = confirm("Click okay if you want to use capital letters");
   
-  //create an object variable to store all the user preferences
+  //make sure that the user chose some type of character 
+  if(hasSpecialCharacters === false && 
+      hasNumericCharacters === false &&
+      hasLowerCaseCharacters === false &&
+      hasUpperCaseCharacters === false){
+        alert("Please choose at least one type of character to create your password");
+        return
+      }
+  
+  //create an object variable to store all the user preferences. setting key-value pairs. 
 
   var preferences = {
     length: length,
     hasSpecialCharacters: hasSpecialCharacters,
+    hasNumericCharacters: hasNumericCharacters,
+    hasLowerCaseCharacters: hasLowerCaseCharacters,
+    hasUpperCaseCharacters: hasUpperCaseCharacters,
 
   }
   console.log(preferences);
@@ -110,20 +125,75 @@ function getPasswordOptionsFromUser() {
   return preferences;
 }
 
+
+//Create a helper function that grabs a random character (index) from an array. 
+function getRandom(ar) {
+  //grab a random index for the array 
+  var randomIndex = Math.floor(Math.random() * ar.length);
+  //using the random Index, get a random element from the array 
+  var randomElement = ar[randomIndex];
+  return randomElement;
+
+}
+
+
+//Now that we have user preferences, we can build their password! 
 function generatePassword() {
 
-
+//Received user preferences, 
 var options = getPasswordOptionsFromUser();
-console.log(options); 
 
-
-//use array 'concat' method
-
+//Where we store the users password. 
 var password = [];
 
+//Storing the characters the user could potentially use, based on their preferences. 
 var possibleCharacters = [];
 
+//A place to store a few characters so we know we have a few different characters... 
 var guaranteedCharacters = [];
+
+//If there are no user preferences, escape from the function! 
+if(!options){
+  return 
+}
+
+//Starting to build our possibleCharacters arrays, 
+if(options.hasSpecialCharacters){
+  //push the special characters into the possible characters array 
+  possibleCharacters = possibleCharacters.concat(specialCharacters)
+  //console.log(possibleCharacters);
+
+}
+if(options.hasNumericCharacters){
+  //push the numeric characters into the possible characters array 
+  possibleCharacters = possibleCharacters.concat(numericCharacters)
+  //console.log(possibleCharacters);
+
+}
+if(options.hasLowerCaseCharacters){
+  //push the lower case characters into the possible characters array 
+  possibleCharacters = possibleCharacters.concat(lowerCaseCharacters)
+  //console.log(possibleCharacters);
+
+}
+if(options.hasUpperCaseCharacters){
+  //push the upper case characters into the possible characters array 
+  possibleCharacters = possibleCharacters.concat(upperCaseCharacters)
+  console.log(possibleCharacters);
+
+}
+
+
+//Loop over the possible characters array, as many times as the length of users password 
+for(var i = 0; i < options.length; i++) {
+  //every time we loop over, grab a random character from the array 
+  var randomCharacter = getRandom(possibleCharacters); 
+  //push the random character into the password array 
+  password.push(randomCharacter); 
+  console.log(password)
+
+  }
+
 
 return password.join("");
 
